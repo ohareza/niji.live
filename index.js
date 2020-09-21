bcd468a4a0a50c235fc9ab0e4781def2bdb6777e
Reza <67229160+ohareza@users.noreply.github.com>
2020-09-21 07:57:55 +0700

    Add error handling for API call

--- index.js
+++ index.js
@@ -13,37 +13,38 @@ const channels = [
   "UC8Snw5i4eOJXEQqURAK17hQ",
   "UCoWH3sDpeXG1aXmOxveX4KA",
   "UCyRkQSuhJILuGOuXk10voPg",
-  "UCk5r533QVMgJUdWwqegH2TA"
+  "UCk5r533QVMgJUdWwqegH2TA",
 ];
-  
-const api =
-  "https://api.chooks.app/videos?status=live,upcoming&group=nijisanji";
+
+const api = "https://api.chooks.app/videos?status=live,upcoming&group=nijisanji";
 
 function yt(id) {
   return Response.redirect("https://www.youtube.com/watch?v=" + id, 307);
 }
 
 async function handleRequest() {
-  const request = await fetch(api, { cf: { cacheTtl: 60 }}) 
+  const request = await fetch(api, { cf: { cacheTtl: 60 } });
+  if (!request.ok) return new Response("live machine broke", { status: 503 });
+
   const videos = await request.json();
 
   let min_start_time = Infinity;
   let min_scheduled_time = Infinity;
   let live_id, scheduled_id;
 
-  videos.forEach( video => {
+  videos.forEach(video => {
     if (!channels.includes(video.channel)) return;
 
-    if (video.status == 'live' && video.start_time < min_start_time) {
+    if (video.status == "live" && video.start_time < min_start_time) {
       min_start_time = video.start_time;
       live_id = video.id;
     }
 
-    if (live_id || video.status == 'upcoming' && video.scheduled_time < min_scheduled_time) {
+    if (live_id || (video.status == "upcoming" && video.scheduled_time < min_scheduled_time)) {
       min_scheduled_time = video.scheduled_time;
       scheduled_id = video.id;
     }
-  })
+  });
 
   if (live_id) return yt(live_id);
   if (scheduled_id) return yt(scheduled_id);
